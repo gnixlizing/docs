f452af894ec56ddf907cab877959b62fe2a6b2c8
gnixing <gnixing@yeah.net>
2017-02-13 14:53:51 +0800

    landing

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 self.$config = {
     // use the readme in repo
     home: 'https://raw.githubusercontent.com/egoist/docute/master/README.md',
-   nav:[
-       {title:'Home',path:'/'},
-       {title:'中文文档',path:'/language/chinese'}
-   ]
+    landing:true,
+    nav: [
+        {title: 'Home', path: './README.md'},
+        {title: '中文文档', path: '/language/chinese'}
+    ]
 };
