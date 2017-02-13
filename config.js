ada75c85191efefdffd9f0a38c9e4e56a933a64a
gnixing <gnixing@yeah.net>
2017-02-13 15:12:36 +0800

    pms

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 self.$config = {
     // use the readme in repo
     home: 'https://raw.githubusercontent.com/egoist/docute/master/README.md',
+    landing:'./README.md',
     nav: [
-        {title: 'Home', path: './README.md'},
+        {title: 'Home', path: '/home'},
         {
             title: 'PMS', type: 'dropdown', items: [
-            {title: 'PMS对接注意事项', path: './PMS/PMS对接注意事项.md'}
+            {title: 'PMS对接注意事项',type:'label', path: './PMS/PMS对接注意事项.md'}
         ]
         }
     ]
