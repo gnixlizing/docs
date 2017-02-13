f5ba70b79e17198be5783d1cae47357be353b1a5
gnixing <gnixing@yeah.net>
2017-02-13 15:22:25 +0800

    NAV

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ self.$config = {
     home: 'https://raw.githubusercontent.com/egoist/docute/master/README.md',
     landing: './README.md',
     nav: [
-        {title: 'Home', path: '/home'},
+        {title: 'Home', path: '/README.md'},
         {
             title: 'PMS', type: 'dropdown', items: [
 
-            {title: 'PMS对接注意事项', path: './PMS/PMS对接注意事项.md'},
+            {title: 'PMS对接注意事项', path: '/PMS/PMS对接注意事项.md'},
             {type: 'sep'},
-            {title: '别样红PMS对接', path: './PMS/别样红PMS对接.md'},
-            {title: '绿云PMS对接', path: './PMS/绿云PMS对接.md'}
+            {title: '别样红PMS对接', path: '/PMS/别样红PMS对接.md'},
+            {title: '绿云PMS对接', path: '/PMS/绿云PMS对接.md'}
         ]
         }
     ]
