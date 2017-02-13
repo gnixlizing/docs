5778259aa3f80601bca8217a361ff69e5a0bd539
gnixing <gnixing@yeah.net>
2017-02-13 15:33:52 +0800

    NAV

--- config.js
+++ config.js
@@ -4,9 +4,8 @@ self.$config = {
     nav: [
         {title: 'Home', path: '/README.md'},
         {
-            title: 'PMS', type: 'dropdown', items: [
-
-            {title: 'PMS对接注意事项', path: '/PMS/PMS对接注意事项.md'},
+            title: 'PMS', type: 'dropdown', exact:true, items: [
+            {title: 'PMS对接注意事项',path: '/PMS/PMS对接注意事项.md'},
             {type: 'sep'},
             {title: '别样红PMS对接', path: '/PMS/别样红PMS对接.md'},
             {title: '绿云PMS对接', path: '/PMS/绿云PMS对接.md'}
